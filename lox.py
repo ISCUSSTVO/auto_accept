702f3bd0e235f069614eba4c9dea12b364eb1fc4
ISCUSSTVO <152444700+ISCUSSTVO@users.noreply.github.com>
2024-11-07 11:26:28 +1000

    рабочий

--- lox.py
+++ lox.py
@@ -1,4 +1,5 @@
 import asyncio
+import threading
 from aiogram import Bot, Dispatcher
 import os
 import cv2
@@ -8,11 +9,9 @@ import mss
 import time
 import tkinter as tk
 import subprocess
-from aiogram.fsm.storage.memory import MemoryStorage
 
 
 bot = Bot(token='7697418130:AAFY4SalSyHyIhSKrh8ubeDaU4GlMHjhsas')  # Замените на ваш токен
-storage = MemoryStorage()
 dp = Dispatcher()  # Используйте именованный аргумент для storage
 
 async def send_telegram_message(message):
@@ -35,12 +34,15 @@ def build_executable():
 
 def update_message(message):
     message_box.config(state=tk.NORMAL)
-    message_box.insert(tk.END, message + "n")
+    message_box.insert(tk.END, message + "\n")
     message_box.see(tk.END)
     message_box.config(state=tk.DISABLED)
 
 async def main_loop():
     global button_found
+    
+    sct = mss.mss()
+    button_found = False
     while True:
         screenshot = sct.grab(sct.monitors[1])
         screenshot_np = np.array(screenshot)
@@ -67,7 +69,7 @@ def start_async_loop():
     asyncio.run(main_loop())
 
 if __name__ == "__main__":
-    build_executable()
+    #build_executable()
 
     button_image_path = r'C:\Users\nikis\Desktop\auto_accept\accept_button.png'
     
@@ -83,18 +85,19 @@ if __name__ == "__main__":
 
     button_height, button_width, _ = button_image.shape
 
-    sct = mss.mss()
-    button_found = False
+    
 
     root = tk.Tk()
     root.title("auto_accept")
-    root.geometry("300x200")
+    root.geometry("300x300")
+    #button_stop_start = tk.Button(root, text='Стоп', command =  width=100)
+
 
     message_box = tk.Text(root, state=tk.DISABLED)
     message_box.pack(expand=True, fill=tk.BOTH)
     update_message("Поиск")
 
     # Запускаем асинхронный цикл в отдельном потоке
-    asyncio.run_coroutine_threadsafe(start_async_loop(), asyncio.get_event_loop())
+    threading.Thread(target=start_async_loop, daemon=True).start()
 
     root.mainloop()  # Запуск GUI
